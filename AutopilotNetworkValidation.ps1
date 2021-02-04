245e67673fcfd885f2a641164e6b6b48a081c7a7
Adam <40191591+Mauvlans@users.noreply.github.com>
2021-02-03 20:40:35 -0700

    Add files via upload

--- AutopilotNetworkValidation.ps1
+++ AutopilotNetworkValidation.ps1
@@ -1,4 +1,18 @@
-
+# ***************************************************************************
+#
+# Purpose: Script to Validate Endpoint avaliblilty for Autopilot. 
+#
+# ------------- DISCLAIMER -------------------------------------------------
+# This script code is provided as is with no guarantee or waranty concerning
+# the usability or impact on systems and may be used, distributed, and
+# modified in any way provided the parties agree and acknowledge the 
+# Microsoft or Microsoft Partners have neither accountabilty or 
+# responsibility for results produced by use of this script.
+#
+# Microsoft will not provide any support through any means.
+# ------------- DISCLAIMER -------------------------------------------------
+#
+# ***************************************************************************
 
 $hash = @{
     'go.microsoft.com' = '443'
